2cdf5ede8e6b9fb6c2c4654e58954afffb04c7a7
Nicholas Page <npage85@gmail.com>
2017-11-22 22:48:59 -0800

    Got Github authentication working.

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -2,6 +2,28 @@
 
 class WebhookController < ApplicationController
   def handle_webhook
-    render :webhook
+    request.body.rewind
+    payload_body = request.body.read
+    puts payload_body
+
+    if payload_authorized?(payload_body)
+      render :webhook
+    else
+      render json: {
+        error: 'Access denied',
+        status: :unauthorized
+      }
+    end
+  end
+
+  private
+
+  def payload_authorized?(payload_body)
+    signature = 'sha1=' + OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new('sha1'), Rails.application.secrets.webhook_secret, payload_body)
+
+    puts signature if Rails.env.development?
+    puts request.headers['HTTP_X_HUB_SIGNATURE'].to_s if Rails.env.development?
+
+    Rack::Utils.secure_compare(signature, request.headers['HTTP_X_HUB_SIGNATURE'].to_s)
   end
 end
